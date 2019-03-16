bf11a342a6b11d144828094caf91127a5c3466e0
chandni <chandnibpatel@gmail.com>
2019-03-16 11:09:51 -0500

    backend

---
+++ reservedata.js
@@ -0,0 +1,9 @@
+var reservations= [
+    {
+        customerName: "Test",
+        phoneNumber: "5129999999",
+        customerEmail: "cs@test.com",
+        customerID: "1000001"
+    }
+]
+module.exports = reservations;
---
+++ waitlistdata.js
@@ -0,0 +1,10 @@
+var waitingList= [
+    {
+        customerName: "Wait1",
+        phoneNumber: "5129999999",
+        customerEmail: "cs@test.com",
+        customerID: "test"
+    }
+]
+
+module.exports = waitingList;
